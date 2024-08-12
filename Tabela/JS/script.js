1ed95bf32da1c8643ece201211c7cad0cc57b747
Isaque Roberto <71456433+IsaqueR02@users.noreply.github.com>
2024-08-12 09:40:02 -0300

    Delete Tabela directory

--- Tabela/JS/script.js
+++
@@ -1,8 +0,0 @@
-const text = document.querySelector("textarea")
-
-text.addEventListener("keyup", e =>{
-  text.style.height = "auto";
-  let scHeight = e.target.scrollHeight;
-  text.style.height = `${scHeight}px`;
-  console.log(text)
-});
